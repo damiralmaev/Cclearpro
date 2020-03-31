0cd778116adf91577328d4515ecfbd056653ce3d
damiralmaev <damiralmaev@gmail.com>
2020-03-31 13:16:20 +0300

    Добавил консоль!

---
+++ Console/Ccleapro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ccleapro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Cclearpro";
+            Console.Write("Лол: ");
+            Console.ReadLine();
+        }
+    }
+}
